32354d1c38ea458367f46a85bffdebe42ae794c2
mobeenaftab <mobeenaftab1@gmail.com>
2019-02-12 17:21:51 +0000

    Create mongoQuries.js
    new mongo query

---
+++ CW1/mongoQuries.js
@@ -0,0 +1,39 @@
+// Get the employee associated with a customer using left outer join
+/*
+db.getCollection("customers").aggregate({
+	$lookup: {
+		from: 'employees',
+		localField: 'SupportRepId',
+		foreignField: 'EmployeeId',
+		as: 'SupportEmployee'
+	}
+})
+*/
+
+// Get the total ammount each CustomerId has spent on the web application
+/*db.getCollection("customers").aggregate(
+	{
+		$group: {
+			_id: '$CustomerId',
+			TotalBill: { $sum: '$Total'}
+		}
+	},
+	{ $sort: { TotalBill: 1 } }
+)
+*/
+db.getCollection("customers").aggregate([
+{
+	$lookup: {
+		from: 'tracks',
+		localField: 'TrackId',
+		foreignField: 'TrackId',
+		as: 'NewTrack'
+	}
+},
+{
+	$replaceRoot: {newRoot: {$mergeObjects: [{$arrayElemAt: ['$NewTrack', 0]}, '$$ROOT']}}
+},
+{ $project: {NewTrack: 0}}
+])
+
+
